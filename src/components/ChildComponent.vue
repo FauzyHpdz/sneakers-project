b40824a5b3301fdb35f8a46d0c27a4a27ba2ae19
fauzihpdz_ <98862368+FauzyHpdz@users.noreply.github.com>
2024-06-02 04:35:37 +0700

    Add files via upload

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex justify-center">
+    <input
+      v-model="taskName"
+      @keyup.enter="submitTask"
+      placeholder="add task"
+      class="flex-1 p-2 border rounded max-w-2xl"
+    />
+    <button @click="submitTask" class="ml-2 p-3 bg-blue-500 text-white rounded">
+      <i class="bi bi-plus"></i>
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const taskName = ref("");
+
+const submitTask = () => {
+  if (taskName.value.trim()) {
+    submitTaskHandler(taskName.value);
+    taskName.value = "";
+  }
+};
+
+// Mendefinisikan prop submitTaskHandler
+const { submitTaskHandler } = defineProps(["submitTaskHandler"]);
+</script>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container mx-auto p-4">
+    <h1 class="text-2xl font-bold mb-4 text-center p-10">TASK LIST</h1>
+    <!-- Meneruskan fungsi memancarkan event ke komponen TaskForm -->
+    <task-form :submitTaskHandler="addTask"></task-form>
+    <div class="flex justify-center mt-4">
+      <ul class="w-full max-w-2xl">
+        <li
+          v-for="task in tasks"
+          :key="task.id"
+          class="bg-gray-100 rounded p-2 mb-2 shadow flex justify-between items-center"
+        >
+          <span>{{ task.name }}</span>
+          <button
+            @click="deleteTask(task.id)"
+            class="ml-2 p-2 bg-red-500 text-white rounded"
+          >
+            <i class="bi bi-x"></i>
+          </button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import TaskForm from "./ChildComponent.vue";
+
+const tasks = ref([]);
+
+const addTask = (newTask) => {
+  tasks.value.push({
+    id: tasks.value.length + 1,
+    name: newTask,
+  });
+};
+
+const deleteTask = (taskId) => {
+  tasks.value = tasks.value.filter((task) => task.id !== taskId);
+};
+</script>
